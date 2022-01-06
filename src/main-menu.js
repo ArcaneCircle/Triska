03356ec2dbde6ff9aee89fbc3ed9485475cb9631
adbenitez <asieldbenitez@gmail.com>
2022-01-06 03:36:59 -0500

    improve scoreboard

--- src/main-menu.js
+++ src/main-menu.js
@@ -19,10 +19,35 @@ class MainMenu extends Menu {
                 CONFIG.height / 2 + 125,
                 'SCOREBOARD',
                 () => {
-                    let items = "";
-                    highscores().forEach(item => items += '<li><span class="w3-large">' + item.name + '</span><span class="w3-right">' + item.score + '</span></li>');
-                    const ul = document.getElementById('scoreboard-list');
-                    ul.innerHTML = items;
+                    const container = document.getElementById('scoreboard-container');
+                    container.innerHTML = '';
+
+                    const addr = window.webxdc.selfAddr();
+                    const list = document.createElement('ol');
+                    list.className = 'w3-ol';
+                    highscores().forEach(item => {
+                        const name = document.createElement('span');
+                        name.className = 'w3-large';
+                        name.textContent = item.name;
+
+                        const score = document.createElement('span');
+                        score.textContent = item.score;
+                        score.className = 'w3-right';
+
+                        const li = document.createElement('li');
+                        if (item.addr == addr) {
+                            const strong = document.createElement("strong");
+                            strong.appendChild(name);
+                            strong.appendChild(score);
+                            li.appendChild(strong);
+                        } else {
+                            li.appendChild(name);
+                            li.appendChild(score);
+                        }
+                        list.appendChild(li);
+                    });
+
+                    container.appendChild(list);
                     document.getElementById('scoreboard').style.display='block';
                 },
             ));
